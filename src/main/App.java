1fff2b05e8c9a66103d5cafb236a70b4f10cf8ed
GusDamian <damiangustavoangel@gmail.com>
2023-11-07 19:59:51 -0600

    02 Ordenando archivos

--- src/main/App.java
+++ src/main/App.java
@@ -1,5 +1,11 @@
+package main;
+
+import methods.Methods;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Methods methods = new Methods();
+        
     }
 }
